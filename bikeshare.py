ccead6ae7b93da4e03a92ac606778ab854947565
wangwen3 <wen.wangwe@bshg.com>
2024-06-03 18:50:42 +0800

    Refactor print info and update readme

--- bikeshare.py
+++ bikeshare.py
@@ -14,7 +14,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! refactoring branch Let\'s explore some US bikeshare data!')
     
     cities = ['chicago', 'new york city', 'washington']
     while True:
